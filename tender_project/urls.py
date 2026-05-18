954b11dbffb09e946cbbf892e62aad1a953e38d9
Hemish123 <consultant@jmsadvisory.in>
2026-05-18 17:05:11 +0530

    media files

--- tender_project/urls.py
+++ tender_project/urls.py
@@ -2,9 +2,9 @@
 URL configuration for tender_project project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from tenders.views import UploadPageView
 
 urlpatterns = [
@@ -13,6 +13,8 @@ urlpatterns = [
     path('api/', include('tenders.urls')),
 ]
 
-# Serve uploaded media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve uploaded media files (PDFs) both in development and production
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
